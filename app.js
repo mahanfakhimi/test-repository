ca67049abc97963e23acbb168e3606d281d77727
mahan <dev.mahanfakhimi@gmail.com>
2024-01-08 00:25:54 +0330

    add new feature

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const userAges = [1, 55, 56, 18, 25, 36, 45, 48, 13, 18];
 
-const sumAges = userAges.reduce((acc, age) => acc + age, 0);
+const sumAges = userAges.reduce((acc, age) => acc + age * 2, 0);
 
 console.log(sumAges);
